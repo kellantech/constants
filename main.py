7e540eb5c0a5d361ebc535fbcd7482138682634d
Kellan Butler <87137401+kellantech@users.noreply.github.com>
2022-05-21 16:29:05 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from decimal import Decimal, getcontext
+import conststr
+
+def pi(n):
+	# only supports 15 decimals
+	pi = float(conststr.pi[:(n+2)])
+	return pi
+def pi_dec(n):
+	# suports all 1000 didgets
+	pi = Decimal(conststr.pi[:(n+2)])
+	return pi
+def phi(n):
+	phi = float(conststr.phi[:(n+2)])
+	return phi
+def phi_dec(n):
+	phi = Decimal(conststr.phi[:(n+2)])
+	return phi
+
+def e_(n):
+	return  float(conststr.e[:(n+2)])
+def e_dec(n):
+	return Decimal(conststr.e[:(n+2)])
+
+
